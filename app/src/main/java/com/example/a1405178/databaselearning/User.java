ec6f1c59fa6d97175434e85a06ac05d37b7f5708
Zusia <sushmita.roy96@gmail.com>
2022-01-24 17:36:28 +0530

    learning databases - commit

---
+++ app/src/main/java/com/example/a1405178/databaselearning/User.java
@@ -0,0 +1,23 @@
+package com.example.a1405178.databaselearning;
+
+public class User {
+
+    /**
+     * Created by amardeep on 10/26/2017.
+     */
+
+
+        public String id;
+        public String userName;
+        public String email;
+        public String password;
+
+        public User(String id, String userName, String email, String password) {
+            this.id = id;
+            this.userName = userName;
+            this.email = email;
+            this.password = password;
+        }
+
+    }
+
